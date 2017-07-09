f62c0181ac3dbf5c6c855923d88e76c402383d29
roeuihyun <roeuihyun@gmail.com>
2017-07-09 17:05:13 +0900

    Console input/output practice

--- src/HelloCpp.cpp
+++ src/HelloCpp.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main() {
 
+	//cout == console out meaning
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
 	int a;		// 정수형 변수 선언 (ex: -1, 0, 1, 2 ...)
@@ -39,9 +40,25 @@ int main() {
 	cout << "pi : "<< pi << endl; // prints pi
 
 	for(i=0;i<10;i++){
-		cout << i << endl;
+		cout << i ;
 	}
+	cout << "i : " << i << endl;
 
+    int sum = 0;
+
+    for(i=1;i<=10;i++){
+        sum += i;
+    }
+
+    cout << "합은 : " << sum << endl;
+
+    char name[10];
+
+    //cin == console in java의 Scanner System.in 이라 생각하면 편
+    cout << "이름을 입력하시고 엔터를 치세요 : ";
+    cin >> name;
+
+    cout << "당신의 이름은 : " << name << " 입니다. "<< endl;
 	return 0;
 
 }
